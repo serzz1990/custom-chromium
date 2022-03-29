853f22af6efc91a5a49e5ef642fe0c60a4007bf7
sergeysavin <serzz1990@gmail.com>
2022-03-29 19:31:50 +0300

    upd for latest chromium version

--- builder/cmd/setup.js
+++ builder/cmd/setup.js
@@ -20,7 +20,10 @@ const copy = require("../utils/copy");
         if (!fs.existsSync(configPath.chromiumPath)) {
           fs.mkdirSync(configPath.chromiumPath, { recursive: true });
           shell.cd(configPath.chromiumPath);
-          depotTools.cmd('caffeinate fetch --no-history chromium');
+          // https://chromium.googlesource.com/chromium/src.git#:~:text=Chromium%20is%20an%20open%2Dsource,how%20to%20get%20the%20code.
+          // shell.exec('git fetch https://chromium.googlesource.com/chromium/src.git +refs/tags/59.0.3071.115:chromium_59.0.3071.115 --depth 1');
+          // depotTools.cmd('caffeinate fetch --no-history chromium');
+          depotTools.cmd('caffeinate fetch chromium');
         }
       }
     },
